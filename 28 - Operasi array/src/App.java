30428667646e201ff2c707c1ec8023189fc71ad7
Dani Orlando <daniorlando8@gmail.com>
2021-11-25 13:03:18 +0700

    25/11/2021

--- 28 - Operasi array/src/App.java
+++ 28 - Operasi array/src/App.java
@@ -33,3 +33,24 @@ class App {
 
 	}
 }
+/**
+ * Operasi Array
+Hampir setiap operasi array dapat kita lakukang dengan memanfaatkan perulangan for. Selain itu kita juga akan membutuhkan
+beberapa operator seperti relasional dan assignment.
+
+Sekarang kita akan mencoba membuat beberapa operasi sederhana terhadap array yang berisi angka. Beberapa operasi yang 
+akan kita lakukan adalah operasi max, min, sum, dan average.
+
+Praktek
+Pada editor terdapat contoh program berisi array angka dan juga beberapa operasi sederhana diantaranya sum, min, dan max.
+Silakan eksekusi kode tersebut dan lihat output yang dihasilkan.
+Dapatkah Kamu menghitung nilai rata-rata/average dari nilai scores? Nilai rata-rata didapatkan dari total nilai dibagi 
+banyaknya nilai. Untuk mendapatkan banyaknya nilai, gunakan scores.length.
+
+total = 0;
+for (int val: scores){
+    total += val;
+}
+System.out.println("Average of scores: " + ( total / scores.length ) );
+
+ */
